b1c6cf5e9e77a55c979781ea766ba309e9004b03
young <mryuanyoung@outlook.com>
2022-03-16 15:58:16 +0800

    chore: 添加lint规则和git hook
    Signed-off-by: young <mryuanyoung@outlook.com>

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: '0.0.0.0'
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components/'),
+      '@hooks': path.resolve(__dirname, './src/hooks/'),
+      '@constants': path.resolve(__dirname, './src/constants/'),
+      '@assets': path.resolve(__dirname, './src/assets/'),
+      '@pages': path.resolve(__dirname, './src/pages/'),
+      '@PO': path.resolve(__dirname, './src/PO/'),
+      '@utils': path.resolve(__dirname, './src/utils/'),
+      '@interfaces': path.resolve(__dirname, './src/interfaces/'),
+      '@api': path.resolve(__dirname, './src/api/')
+    },
+  }
+})
